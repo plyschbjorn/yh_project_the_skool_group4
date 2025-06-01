50100e3817c974ea1fe6768301d160115ce111b1
ebt35 <eyoub.beraki@hotmail.com>
2025-06-01 13:45:09 +0200

    connected course page to main.py

--- main.py
+++ main.py
@@ -6,18 +6,19 @@ from frontend.pages.home import home_page
 from frontend.pages.dashboard import dashboard_page
 import matplotlib.pyplot as plt
 from matplotlib.ticker import FuncFormatter
-
+from frontend.pages.ansök_kurs_omgång import ansökningar, gui_data
 
 
 
 pages = {
-    "home": home_page, "dashboard": dashboard_page , "data": data_page
+    "home": home_page, "kurs": ansökningar, "dashboard": dashboard_page , "data": data_page
 }       
 
 
 
-
-
-
-# === 8. Run GUI
-Gui(pages= pages, css_file= "assets/main.css").run(dark_mode=False, use_reloader=True, port=8050)
+if __name__ == "__main__":
+    Gui(pages=pages, css_file="assets/style.css").run(
+        port=8080,
+        dark_mode=False,
+        use_reloader=False
+    )
